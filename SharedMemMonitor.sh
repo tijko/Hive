8b1ab06413f2670889c0a724cf2c6e554ff8ce05
Tim Konick <konick781@gmail.com>
2024-03-29 17:37:31 -0400

    Verbosity Flag

--- SharedMemMonitor.sh
+++ SharedMemMonitor.sh
@@ -2,27 +2,42 @@
 
 firefox_pid=""
 rss_total=0
+verbose=false
 
-for pid in $(pidof firefox | tr ' ' '\n' | sort -n)
+if [[ $# -gt 2 ]]
+then
+    verbose=true
+fi
+
+for ((;;))
 do
-    # linux/Documentation/filesystems/proc.rst (Status VmRSS)
-    # Contains - (RssAnon + RssShmem + RssFile)    
-    #
-    # This 1st PID will be the PPID of the rest that follow.
-    if [[ $firefox_pid -eq "" ]]
-    then
-        firefox_pid=$pid
-        echo Found Firefox PID $firefox_pid
-    else
-        echo Found Firefox Child $pid
-        cd /proc/$pid/
-        ppid=$(ps -o ppid= -p $pid)
-        if [[ $ppid -eq $firefox_pid ]]
+    for pid in $(pidof firefox | tr ' ' '\n' | sort -n)
+    do
+        # linux/Documentation/filesystems/proc.rst (Status VmRSS)
+        # Contains - (RssAnon + RssShmem + RssFile)    
+        #
+        # This 1st PID will be the PPID of the rest that follow.
+        if [[ $firefox_pid -eq "" ]]
         then
-            rss=$(cat status | awk -F':' '{if ($1 == "VmRSS") print $2}' | tr -d 'kB')
-            rss_total=$(($rss + $rss_total))
+            firefox_pid=$pid
+            if [[ "${verbose}" == true ]]
+            then
+                echo Found Firefox PID $firefox_pid
+            fi
+        else
+            if [[ "${verbos}" == true ]]
+            then
+                echo Found Firefox Child $pid
+            fi
+            cd /proc/$pid/
+            ppid=$(ps -o ppid= -p $pid)
+            if [[ $ppid -eq $firefox_pid ]]
+            then
+                rss=$(cat status | awk -F':' '{if ($1 == "VmRSS") print $2}' | tr -d 'kB')
+                rss_total=$(($rss + $rss_total))
+            fi
         fi
-    fi
-done
+    done
 
-echo Total Memory $rss_total
+    echo Total Memory ${rss_total}kB
+done
